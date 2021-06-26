1b1d2b8747f1365d136121e77499f31f2b30a590
Joel Tchoupe <73880183+jojocam666@users.noreply.github.com>
2021-06-26 13:22:58 +0100

    Update baseclient.py

--- bitcoinlib/services/baseclient.py
+++ bitcoinlib/services/baseclient.py
@@ -1,29 +1,11 @@
-# -*- coding: utf-8 -*-
-#
-#    BitcoinLib - Python Cryptocurrency Library
-#    Base Client
-#    © 2016 November - 1200 Web Development <http://1200wd.com/>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
+
 
 import requests
 from urllib.parse import urlencode
 import json
-from bitcoinlib.main import *
-from bitcoinlib.networks import Network
-from bitcoinlib.keys import Address
+from wialib.main import *
+from wialib.networks import Network
+from wialib.keys import Address
 
 _logger = logging.getLogger(__name__)
 
@@ -65,10 +47,10 @@ class BaseClient(object):
         if not url or not self.base_url:
             raise ClientError("No (complete) url provided: %s" % url)
         headers = {
-            'User-Agent': 'BitcoinLib/%s' % BITCOINLIB_VERSION,
+            'User-Agent': 'WiaLib/%s' % WIALIB_VERSION,
             'Accept': 'application/json',
             # 'Content-Type': 'application/json',
-            "Referrer": "https://www.github.com/1200wd/bitcoinlib",
+            "Referrer": "https://www.github.com/1200wd/wialib",
         }
         # ToDo: Check use 'headers = None' for some providers?
         if method == 'get':
