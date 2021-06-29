92d5075390fa41119b4e33d297fc9442f41e4eeb
Joel Tchoupe <73880183+jojocam666@users.noreply.github.com>
2021-06-29 11:32:16 +0100

    Update bitgo.py

--- bitcoinlib/services/bitgo.py
+++ bitcoinlib/services/bitgo.py
@@ -1,27 +1,9 @@
-# -*- coding: utf-8 -*-
-#
-#    BitcoinLib - Python Cryptocurrency Library
-#    BitGo Client
-#    © 2017-2019 July - 1200 Web Development <http://1200wd.com/>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
+
 
 import logging
 from datetime import datetime
-from bitcoinlib.main import MAX_TRANSACTIONS
-from bitcoinlib.services.baseclient import BaseClient, ClientError
+from wialib.main import MAX_TRANSACTIONS
+from wialib.services.baseclient import BaseClient, ClientError
 from bitcoinlib.transactions import Transaction
 
 _logger = logging.getLogger(__name__)
