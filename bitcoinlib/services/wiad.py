868e87a8576ccb27dc5e809f7991da8194ee7cf9
Joel Tchoupe <73880183+jojocam666@users.noreply.github.com>
2021-06-29 11:20:38 +0100

    Update and rename bitcoind.py to wiad.py

--- bitcoinlib/services/wiad.py
+++ bitcoinlib/services/wiad.py
@@ -1,32 +1,14 @@
-# -*- coding: utf-8 -*-
-#
-#    BitcoinLib - Python Cryptocurrency Library
-#    Client for bitcoind deamon
-#    © 2017 - 2020 Oct - 1200 Web Development <http://1200wd.com/>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
+
 
 import configparser
-from bitcoinlib.main import *
-from bitcoinlib.services.authproxy import AuthServiceProxy
-from bitcoinlib.services.baseclient import BaseClient, ClientError
-from bitcoinlib.transactions import Transaction
-from bitcoinlib.networks import Network
+from wialib.main import *
+from wialib.services.authproxy import AuthServiceProxy
+from wialib.services.baseclient import BaseClient, ClientError
+from wialib.transactions import Transaction
+from wialib.networks import Network
 
 
-PROVIDERNAME = 'bitcoind'
+PROVIDERNAME = 'wiad'
 
 _logger = logging.getLogger(__name__)
 
