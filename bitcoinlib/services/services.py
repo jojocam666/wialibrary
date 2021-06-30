e9df2ae272ff5f5d6ffc7257f5c52e5b0ca8eb96
Joel Tchoupe <73880183+jojocam666@users.noreply.github.com>
2021-06-30 09:34:42 +0100

    Update services.py

--- bitcoinlib/services/services.py
+++ bitcoinlib/services/services.py
@@ -1,34 +1,15 @@
-# -*- coding: utf-8 -*-
-#
-#    BitcoinLib - Python Cryptocurrency Library
-#    SERVICES - Main Service connector
-#    © 2017 - 2021 March - 1200 Web Development <http://1200wd.com/>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
 
 import json
 import random
 import time
 from datetime import timedelta
 from sqlalchemy import func
-from bitcoinlib import services
-from bitcoinlib.networks import Network
-from bitcoinlib.encoding import to_bytes, int_to_varbyteint, varstr, varbyteint_to_int
-from bitcoinlib.db_cache import *
-from bitcoinlib.transactions import Transaction, transaction_update_spents
-from bitcoinlib.blocks import Block
+from wialib import services
+from wialib.networks import Network
+from wialib.encoding import to_bytes, int_to_varbyteint, varstr, varbyteint_to_int
+from wialib.db_cache import *
+from wialib.transactions import Transaction, transaction_update_spents
+from wialib.blocks import Block
 
 
 _logger = logging.getLogger(__name__)
@@ -84,7 +65,7 @@ class Service(object):
             self.network = Network(network)
         if min_providers > max_providers:
             max_providers = min_providers
-        fn = Path(BCL_DATA_DIR, 'providers.json')
+        fn = Path(WIL_DATA_DIR, 'providers.json')
         f = fn.open("r")
 
         try:
