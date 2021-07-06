e9f86a72b028230331ecbfcc7aeeac7f8cf343f7
Joel Tchoupe <73880183+jojocam666@users.noreply.github.com>
2021-07-06 15:53:59 +0100

    Update mine_blocks.py

--- bitcoinlib/bpsc/mine_blocks.py
+++ bitcoinlib/bpsc/mine_blocks.py
@@ -1,1 +1,14 @@
 
+import hashlib
+import json
+from wialib import blocks
+
+class Smart_Blockchain:
+    def __init__(self):
+        self.chain = []
+        when as_dict() return block_dict
+      
+        
+
+        self.chain.append(block_dict)
+        return chain
