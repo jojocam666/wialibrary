a8fb75607574acde2f3e56d0ca763c4bc0a96600
Joel Tchoupe <73880183+jojocam666@users.noreply.github.com>
2021-07-14 13:24:17 +0100

    Update coinbase.py

--- bitcoinlib/bpsc/coinbase.py
+++ bitcoinlib/bpsc/coinbase.py
@@ -31,8 +31,12 @@
 #def divide_coinbase(): #//in order to determine the amount of the rewarding of the best nodes in the blockchain at the last block,excepting the mining contract
 #if node.mining_state = "best": #//the state of the node concerning the mining of the previous_block or last_block
 #   node.rewarding = True
-#   node.reward = self.reward(relay_deposit,relays_n)#// the relay deposit is the amount deposit on the deposit smart contract
+#   node.reward = self.reward(relay_deposit,relays_n,relay_speed)#// the relay deposit is the amount deposit on the "deposit smart contract" in order to prioritize the nodes in the rewarding process; by default instancied at 0.00000
+#   return True
+#if node.status = "masternode":
+#   node.reward = self.masterreward(master_deposit,relays_n,relay_speed)"
+#   return True
 
-#
-#
+#//the reward and masterreward methods are taken from the reward class,as well as the node state(mining_state,standby_state,chain_updating_state,etc) and status defining(normal node or masternode) 
+#directory: node/state/status/reward/activity(banned or not)/relay(concerning the bipms)/deposit
 #
