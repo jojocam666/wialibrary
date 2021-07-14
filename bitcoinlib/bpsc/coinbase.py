0ac3d37dc59cbda01e283b31d0b79d6fe7361634
Joel Tchoupe <73880183+jojocam666@users.noreply.github.com>
2021-07-14 13:01:57 +0100

    Update coinbase.py

--- bitcoinlib/bpsc/coinbase.py
+++ bitcoinlib/bpsc/coinbase.py
@@ -1,1 +1,32 @@
+#Pseudo code of coinbase management and configuration in order to reward the mining contract and the nodes compared to their contribution to the informations relay on the network
+#
+#coinbase = []
+#if new_block() = 1
+#self.next_block = block
+#self.next_block[coinbase = self.coinbase
+#coinbase = Transaction(sender = "0", recipient = [bpsc, best_nodes#//define a method in the coinbase class to determine which node is called best in the mining process//,amount = coinbase_value)
 
+
+# def update req_time():
+#req_time = 0#//the time to spend before updating the cbs value
+    
+#import time
+#start = time.time()
+#new_block()
+#end = time.time(new_block)
+#print(end - start)
+#if req_time = #//1 year:
+#req_time = req_time * 2
+#ti = ti + req_time
+#return a
+#return req_time
+
+#def update_coinbase_value():
+#  coinbase_value = 1000.000000000000000000000000
+#  import update_req_time as urt
+#  if urt.a =+:
+#    coinbase_value = coinbase_value 
+
+ 
+#
+#
